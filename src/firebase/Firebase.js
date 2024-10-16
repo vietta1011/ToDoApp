a8565ecfaffd04bb656bbfb025284326c12dd2d3
vietta1011 <taquocviet1011@gmail.com>
2024-10-16 14:01:08 +0700

    private api key, sua lai background

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAkYP6DIG7CAGBu4VrKcON20tFX_mowEA",
-  authDomain: "todo-app-81594.firebaseapp.com",
-  projectId: "todo-app-81594",
-  storageBucket: "todo-app-81594.appspot.com",
-  messagingSenderId: "141487528216",
-  appId: "1:141487528216:web:fb2420d8979e8096db666d",
-  measurementId: "G-X82BN72RMK",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
